688a9545f9e0625ec06d93dd1fea96f1ea1b7e0e
maxpanev <maxpanev170100@gmail.com>
2024-12-13 14:17:11 +0300

    add new directory

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "myapp" / "static"
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
